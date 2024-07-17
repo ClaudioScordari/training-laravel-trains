396c27b2a90574cc39056691d2943cfc1da56f37
Claudio Scordari <scordari22@gmail.com>
2024-07-17 19:57:15 +0200

    Correzioni in create

--- resources/views/trains/show.blade.php
+++ resources/views/trains/show.blade.php
@@ -10,15 +10,51 @@
 <section id="{{ 'treno-numero-' . $train->id }}">
     <ul>
         <li>
-            {{ $train->company }}
+            company: {{ $train->company }}
         </li>
 
         <li>
-            {{ $train->departure_station }}
+            departure_station: {{ $train->departure_station }}
         </li>
 
         <li>
-            {{ $train->arrival_station }}
+            arrival_station: {{ $train->arrival_station }}
+        </li>
+
+        <li>
+            departure_time: {{ $train->departure_time }}
+        </li>
+
+        <li>
+            arrival_time: {{ $train->arrival_time }}
+        </li>
+
+        <li>
+            code: {{ $train->code }}
+        </li>
+
+        <li>
+            num_carriages: {{ $train->num_carriages }}
+        </li>
+
+        <li>
+            {{-- in_time: {{ $train->in_time }} --}}
+
+            @if ($train->in_time == true)
+                In arrivo
+            @else
+                In ritardo
+            @endif
+        </li>
+
+        <li>
+            {{-- deleted: {{ $train->deleted }} --}}
+
+            @if ($train->deletd == true)
+                Cancellato
+            @else
+                Non cancellato
+            @endif
         </li>
     </ul>
 </section>
